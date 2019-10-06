218ac7bc517135947d683ebf22990833b64ec4a8
Tomáš Růt <trut.cz@gmail.com>
2019-10-06 10:16:31 +0200

    Add sample for workarounding new-super-domain cypress behavior

---
+++ cypress-multiple-super-domains/cypress/integration/examples/test.spec.ts
@@ -0,0 +1,60 @@
+describe("AppPart", () => {
+  prepareForCustomSuperDomain();
+
+  describe("Test", () => {
+    let testNumber = 0;
+    let counter = 0;
+
+    beforeEach(() => {
+      cy.then(() => (testNumber = Math.floor(Math.random() * 1000000))).then(
+        () => {
+          cy.writeFile(`test/${testNumber}`, "test");
+          cy.visit(`http://${testNumber}.bobrilstrap-com.nip.io/`);
+        }
+      );
+    });
+
+    it("test1", () => {
+      cy.wrap(++counter).should("equal", 1);
+
+      cy.get("h1")
+        .should($el => expect($el.text().indexOf("nginx") >= 0).to.be.true)
+        .location()
+        .should(url =>
+          expect(
+            parseInt(url.host.substr(0, url.host.indexOf(".") + 1)),
+            `expect ${url.host} to containt ${testNumber}`
+          ).to.equal(testNumber)
+        );
+    });
+
+    it("test2", () => {
+      cy.wrap(++counter).should("equal", 2);
+
+      cy.get("h1")
+        .should($el => expect($el.text().indexOf("nginx") >= 0).to.be.true)
+        .location()
+        .should(url =>
+          expect(
+            parseInt(url.host.substr(0, url.host.indexOf(".") + 1)),
+            `expect ${url.host} to containt ${testNumber}`
+          ).to.equal(testNumber)
+        );
+    });
+  });
+});
+
+function prepareForCustomSuperDomain(): void {
+  it("Prepare for custom Super Domain", () => {
+    cy.log(
+      `*Pre-case to deal with url different from ${Cypress.config("baseUrl")}*`
+    );
+    cy.visit("http://super-domain-workaround.about-gitlab-com.nip.io");
+    cy.location().then(url =>
+      expect(
+        url.host.substr(0, url.host.indexOf(".")),
+        `expect ${url.host} to contains "super-domain-workaround")`
+      ).to.equal("super-domain-workaround")
+    );
+  });
+}
