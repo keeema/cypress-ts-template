a897f382ee756d83d91901007cd30b3c10ee0038
Tomáš Růt <trut.cz@gmail.com>
2019-10-07 09:05:53 +0200

    Improve custom domain solution

--- cypress-multiple-super-domains/cypress/integration/examples/test.spec.ts
+++ cypress-multiple-super-domains/cypress/integration/examples/test.spec.ts
@@ -1,6 +1,30 @@
-describe("AppPart", () => {
-  prepareForCustomSuperDomain();
+function describeForCustomSuperDomain(
+  title: string,
+  fn: (this: Mocha.Suite) => void
+): Mocha.Suite {
+  return describe(title, function(this: Mocha.Suite) {
+    it("Prepare for custom Super Domain", () => {
+      cy.log(
+        `*Pre-case to deal with url different from ${Cypress.config(
+          "baseUrl"
+        )}*`
+      );
 
+      cy.visit("http://super-domain-workaround.about-gitlab-com.nip.io");
+
+      cy.location().then(url =>
+        expect(
+          url.host.substr(0, url.host.indexOf(".")),
+          `expect ${url.host} to contains "super-domain-workaround")`
+        ).to.equal("super-domain-workaround")
+      );
+    });
+
+    fn.call(this);
+  });
+}
+
+describeForCustomSuperDomain("AppPart", () => {
   describe("Test", () => {
     let testNumber = 0;
     let counter = 0;
@@ -43,18 +67,3 @@ describe("AppPart", () => {
     });
   });
 });
-
-function prepareForCustomSuperDomain(): void {
-  it("Prepare for custom Super Domain", () => {
-    cy.log(
-      `*Pre-case to deal with url different from ${Cypress.config("baseUrl")}*`
-    );
-    cy.visit("http://super-domain-workaround.about-gitlab-com.nip.io");
-    cy.location().then(url =>
-      expect(
-        url.host.substr(0, url.host.indexOf(".")),
-        `expect ${url.host} to contains "super-domain-workaround")`
-      ).to.equal("super-domain-workaround")
-    );
-  });
-}
